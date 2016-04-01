512ee2c3a3d90a78be62295fbb73c2d0b6cd4708
Miguel Rodriguez <miglos@hotmail.es>
2016-04-02 00:45:45 +0200

    Result.php page added

--- result.php
+++ result.php
@@ -1,22 +1,38 @@
 <?php
-
 /**
  * @Author: miguel
  * @Date:   2016-03-31 00:15:30
  * @Last Modified by:   miguel
- * @Last Modified time: 2016-03-31 23:38:24
+ * @Last Modified time: 2016-04-01 22:18:57
  */
 
 	if($_POST) {
 		$mylog = $_POST['inputTextarea1'];
-		// echo $mylog;
+		$result = array();
 
-		$input = $mylog;
-		$input = preg_replace("/((\r?\n)|(\r\n?))/", ',', $input);
-		$pieces = explode(',', $input);
+		$input = preg_replace("/((\r?\n)|(\r\n?))/", '**', $mylog);
+		$pieces = explode('**', $input);
 		foreach($pieces as $part) {
-		  print($part) . '<br>';
-		}
+		    $line = explode(' ', $part);
+		    foreach ($line as $value) {
+		    	$total = sizeof($line);
+		    	$data4 = null;
+		    	$data1 = $line[0] . ' ' . $line[1];
+		    	$data2 = $line[2];
+		    	$data3 = $line[3];
+		    	for ($i=4; $i < $total; $i++) { 
+		    		$data4 .= $line[$i] . ' ';
+		    	}
+
+		    	$result = array(
+		    				'date' => $data1, 
+		    				'process_id' => intval($data2),
+		    				'process_time' => $data3,
+		    				'comments' => trim($data4)
+		    				);
+		    }
+	    	var_dump($result);
+		}    
 	}
 ?>
 
