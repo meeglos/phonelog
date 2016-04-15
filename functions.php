731a2c06fd585fe1f3285847113484bc98b4597e
Miguel Rodriguez <miglos@hotmail.es>
2016-04-16 01:31:15 +0200

    Code improvement

--- functions.php
+++ functions.php
@@ -7,32 +7,52 @@
  * @Last Modified time: 2016-04-13 22:54:25
  */
 
-
+/**
+ * @param $carry
+ * @param $item
+ * @return string
+ */
 	function array_concat($carry, $item)
 	{
 	    $carry .= $item . ' ';
 	    return $carry;
 	}
 
-//	function mysql_insert_array($table, $response_array)
-//	{
-//		foreach ($response_array as $field => $value) {
-//			$fields[] = '' . $field . '';
-//			$values[] = "'" . mysqli_real_escape_string($value) . "'";
-//		}
-//		$field_list = implode(',', $fields);
-//		$value_list = implode(', ', $values);
-//
-//
-//		$query = "INSERT INTO '" . $table . "' (" . $field_list . ") VALUES (" . $value_list . ")";
-//		if (!$query) {
-//			$message = mysqli_error();
-//			die($message);
-//		}
+	function mysqlInsertArray($table, $response_array)
+	{
+		$db = Database::getInstance();
+        $mysqli = $db->getConnection();
 
-//		if ($response_array[0] == 1) {
-//			$table = "payments_received";
-//			mysql_insert_array($table, $response_array);
-//		}
+        $fields = getArrayKeys($response_array);
+        $values = getArrayValues($response_array);
+		$query = "INSERT INTO `" . $table . "`($fields) VALUES " . implode (', ', $values);
+
+		if (!$query) {
+			$message = mysqli_error();
+			die($message);
+		}
+        $mysqli->query($query) or exit(mysqli_error());
+
+		/*if ($response_array[0] == 1) {
+			$table = "payments_received";
+			mysql_insert_array($table, $response_array);
+		}*/
 	}
-?>	
+
+    function getArrayKeys($array)
+    {
+        $resultKeys = array();
+        foreach($array as $sub) {
+            $resultKeys = array_merge($resultKeys, $sub);
+        }
+        return implode(', ', array_keys($resultKeys));
+    }
+
+    function getArrayValues($array)
+    {
+        $resultValues = array();
+        foreach($array as $sub) {
+            $resultValues[] = '("' . $sub['call_date'] . '", "' . $sub['client_id'] . '", "' . $sub['call_lapse'] . '", "' . $sub['comment'] . '")';
+        }
+        return $resultValues;
+    }
--- result.php
+++ result.php
@@ -7,44 +7,29 @@
  */
 include('db_connect.php.inc');
 include('functions.php');
+$table = "logs";
 
 	if($_POST) {
-		$mylog = $_POST['inputTextarea1'];
-		$input = preg_replace("/((\r?\n)|(\r\n?))/", '**', $mylog);
-		$pieces = explode('**', $input);
-		$fields = array('date', 'process_id', 'process_time', 'comments');
+        $mylog = $_POST['inputTextarea1'];
+        $input = preg_replace("/((\r?\n)|(\r\n?))/", '**', $mylog);
+        $pieces = explode('**', $input);
+        $fields = array('call_date', 'client_id', 'call_lapse', 'comment');
 
-		foreach($pieces as $part) {
+        foreach ($pieces as $part) {
 
-			$a = explode(' ', $part);
-			$b = array(2, 1, 1);
-			$i = 0;
-			unset($output);
+            $a = explode(' ', $part);
+            $b = array(2, 1, 1);
+            $i = 0;
+            unset($output);
 
-			foreach ($b as $num_elem) {
-				$output[] = rtrim(array_reduce(array_slice($a, $i, $num_elem), "array_concat"));
-				$i += $num_elem;
-			}
+            foreach ($b as $num_elem) {
+                $output[] = rtrim(array_reduce(array_slice($a, $i, $num_elem), "array_concat"));
+                $i += $num_elem;
+            }
 
-			$output[] = rtrim(array_reduce(array_slice($a, array_sum($b), count($a)), "array_concat"));
+            $output[] = rtrim(array_reduce(array_slice($a, array_sum($b), count($a)), "array_concat"));
 
             $result[] = array_combine($fields, $output);
         }
-
-
-        /*		$db = Database::getInstance();
-                $mysqli = $db->getConnection();
-
-                    $daata = substr($other_result,0,-1);
-                    $sql = "INSERT INTO logs (call_date, client_id, call_lapse, comment)  values ";
-                    $sql .= $daata;
-
-                    echo $sql;
-
-                    $mysqli->query($sql) or exit(mysql_error());*/
-	    
-	}
-				echo serialize($result);
-?>
-
-
+    }
+        mysqlInsertArray($table, $result);
